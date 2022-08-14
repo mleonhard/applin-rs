703c7368dd16c2dec6a0a5da09411a15858eb265
Michael Leonhard <michael@leonhardllc.com>
2022-08-14 12:19:10 -0700

    Add server_state_test::key_set_updates.

--- tests/server_state_test.rs
+++ tests/server_state_test.rs
@@ -3,30 +3,110 @@ mod util;
 use applin::data::{Context, Roster};
 use applin::session::{KeySet, SessionSet};
 use applin::testing::{start_for_test, TestClient};
-use applin::widget::{NavPage, Text};
+use applin::widget::{Empty, NavPage, Text};
 use serde_json::json;
 use servlin::reexport::safina_executor::Executor;
 use servlin::reexport::safina_timer;
 use servlin::{Request, Response};
-use std::ops::AddAssign;
+use std::ops::{AddAssign, BitXorAssign};
 use std::sync::Arc;
 use std::time::Duration;
 
-struct ServerState {
-    counter: Roster<u32, ()>,
-    sessions: SessionSet<()>,
-}
-impl ServerState {
-    pub fn new(executor: &Arc<Executor>) -> Self {
-        Self {
-            counter: Roster::new(3).with_cleanup_task(executor),
-            sessions: SessionSet::new(executor),
+#[test]
+pub fn key_set_updates() {
+    struct ServerState {
+        show_page2: Roster<bool, ()>,
+        sessions: SessionSet<()>,
+    }
+    impl ServerState {
+        pub fn new(executor: &Arc<Executor>) -> Self {
+            Self {
+                show_page2: Roster::new(false).with_cleanup_task(executor),
+                sessions: SessionSet::new(executor),
+            }
         }
     }
+    safina_timer::start_timer_thread();
+    let executor = Executor::new(1, 1).unwrap();
+    let server_state = Arc::new(ServerState::new(&executor));
+    let server_state2 = Arc::clone(&server_state);
+    let key_set_fn = move |rebuilder| {
+        let mut key_set = KeySet::new();
+        key_set.add_static_page("/", NavPage::new("Home", Empty::new()));
+        if *server_state2.show_page2.read(rebuilder) {
+            key_set.add_static_page("/page2", NavPage::new("Page 2", Empty::new()));
+        }
+        Ok(key_set)
+    };
+    let server_state3 = Arc::clone(&server_state);
+    let req_handler = move |req: Request| match (req.method.as_str(), req.url.path()) {
+        ("GET", "/") => server_state3
+            .sessions
+            .get_or_new(&req, key_set_fn, || ())?
+            .poll(),
+        ("GET", "/stream") => server_state3
+            .sessions
+            .get_or_new(&req, key_set_fn, || ())?
+            .stream(),
+        ("POST", "/toggle") => {
+            let session = server_state3.sessions.get(&req)?;
+            server_state3
+                .show_page2
+                .write(&session.rpc_context())
+                .bitxor_assign(true);
+            session.rpc_response()
+        }
+        _ => Ok(Response::not_found_404()),
+    };
+    let (url, _receiver) = start_for_test(&executor, req_handler);
+    let only_home =
+        json!({"pages": {"/": {"typ": "nav-page", "title": "Home", "widget": {"typ":"empty"}}}});
+    let home_and_page2 = json!({"pages": {
+        "/": {"typ": "nav-page", "title": "Home", "widget": {"typ":"empty"}},
+        "/page2": {"typ": "nav-page", "title": "Page 2", "widget": {"typ":"empty"}},
+    }});
+    let empty_update = json!({});
+    let add_page2 = json!({"pages": {"/page2": {"typ": "nav-page", "title": "Page 2", "widget": {"typ":"empty"}}}});
+    let remove_page2 = json!({"pages": {"/page2": null}});
+    let poller1 = TestClient::new(&url);
+    assert_eq!(only_home, poller1.poll().unwrap());
+    assert_eq!(empty_update, poller1.poll().unwrap());
+    // Background thread updates state.
+    *server_state.show_page2.write(&Context::Empty) = true;
+    assert_eq!(home_and_page2, TestClient::new(&url).poll().unwrap());
+    assert_eq!(add_page2, poller1.poll().unwrap());
+    assert_eq!(empty_update, poller1.poll().unwrap());
+    // RPC updates state.
+    let poller2 = TestClient::new(&url);
+    poller2.poll().unwrap();
+    let streamer = TestClient::new(&url);
+    let messages = streamer.stream().unwrap();
+    assert_eq!(
+        &remove_page2,
+        &poller1.post_json("/toggle", json!({})).unwrap()
+    );
+    assert_eq!(&remove_page2, &poller2.poll().unwrap());
+    std::thread::sleep(Duration::from_millis(100));
+    assert_eq!(
+        &[home_and_page2, remove_page2],
+        messages.pop_all().as_slice()
+    );
 }
 
 #[test]
 pub fn page_updates() {
+    struct ServerState {
+        counter: Roster<u32, ()>,
+        sessions: SessionSet<()>,
+    }
+    impl ServerState {
+        pub fn new(executor: &Arc<Executor>) -> Self {
+            Self {
+                counter: Roster::new(3).with_cleanup_task(executor),
+                sessions: SessionSet::new(executor),
+            }
+        }
+    }
     safina_timer::start_timer_thread();
     let executor = Executor::new(1, 1).unwrap();
     let server_state = Arc::new(ServerState::new(&executor));
