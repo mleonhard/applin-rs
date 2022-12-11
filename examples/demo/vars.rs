9c9c499e45dcb47ec5298c1676ee0354ed903535
Michael Leonhard <michael@leonhardllc.com>
2022-12-10 19:51:28 -0800

    Clarify demo "Check Vars" page.

--- examples/demo/vars.rs
+++ examples/demo/vars.rs
@@ -10,27 +10,27 @@ pub fn check_vars_rpc(state: &Arc<ServerState>, req: &Request) -> Result<Respons
     struct Vars {
         // TODO: Find a way to catch typos in variable names.
         #[serde(skip_serializing_if = "Option::is_none")]
-        vars_option_a: Option<bool>,
+        option_a: Option<bool>,
         #[serde(skip_serializing_if = "Option::is_none")]
-        vars_option_b: Option<bool>,
+        option_b: Option<bool>,
     }
     let session = state.sessions.get(req)?;
     let input: Vars = req.json()?;
     let mut output = Vars::default();
-    if input.vars_option_b.unwrap_or(false) && !input.vars_option_a.unwrap_or(false) {
-        output.vars_option_a = Some(true);
+    if !input.option_a.unwrap_or(false) {
+        output.option_b = Some(false);
     }
     session.rpc_response_with_vars(output)
 }
 
 pub fn add_check_vars_page(keys: &mut KeySet<SessionState>) -> PageKey {
     keys.add_static_page(
-        "/vars/check-vars",
+        "/check-vars",
         NavPage::new(
             "Check Vars",
             Form::new((
-                Checkbox::new("vars_option_a", "Option A").with_rpc(CHECK_VARS_RPC_PATH),
-                Checkbox::new("vars_option_b", "Option B (requires Option A)")
+                Checkbox::new("option_a", "Option A").with_rpc(CHECK_VARS_RPC_PATH),
+                Checkbox::new("option_b", "Option B (requires Option A)")
                     .with_rpc(CHECK_VARS_RPC_PATH),
             )),
         ),
