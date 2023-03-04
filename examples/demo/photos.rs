bdf1abfff913224bc7bbc5985202c750e3d7fc2f
Michael Leonhard <michael@leonhardllc.com>
2023-03-04 10:09:52 -0800

    Upload photo demo to return rpc_response so page updates right away.

--- examples/demo/photos.rs
+++ examples/demo/photos.rs
@@ -1,4 +1,4 @@
-use crate::{ServerState, Session};
+use crate::{ServerState, Session, GET_PHOTO_PATH};
 use applin::session::{PageKey, PageMap};
 use applin::widget::{Image, NavPage, Text};
 use servlin::{ContentType, Request, Response};
@@ -17,14 +17,17 @@ pub fn upload_photo_handler(state: &Arc<ServerState>, req: &Request) -> Result<R
         return Ok(Response::get_body_and_reprocess(10 * 1024 * 1024));
     }
     let mut body_bytes = Vec::new();
-    req.body.reader()?.read_to_end(&mut body_bytes)?;
-    let session_guard = applin_session
-        .value
-        .lock()
-        .unwrap_or_else(PoisonError::into_inner);
-    let mut photo_guard = session_guard.photo.write(applin_session.rpc_context());
-    *photo_guard = Some(body_bytes);
-    Ok(Response::new(200))
+    let mut reader = req.body.reader()?;
+    reader.read_to_end(&mut body_bytes)?;
+    {
+        let session_guard = applin_session
+            .value
+            .lock()
+            .unwrap_or_else(PoisonError::into_inner);
+        let mut photo_guard = session_guard.photo.write(applin_session.rpc_context());
+        *photo_guard = Some(body_bytes);
+    }
+    applin_session.rpc_response()
 }
 
 pub fn get_photo_handler(state: &Arc<ServerState>, req: &Request) -> Result<Response, Response> {
@@ -55,7 +58,7 @@ pub fn add_view_photo_page(keys: &mut PageMap<Session>) -> PageKey {
             "Photo",
             match *photo_guard {
                 None => Text::new("No photo found.").to_widget(),
-                Some(..) => Image::new(1.0, "/photo").to_widget(),
+                Some(..) => Image::new(1.0, GET_PHOTO_PATH).to_widget(),
             },
         )
         .with_stream())
